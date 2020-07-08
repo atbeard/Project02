27607bcc40729fc040e3b5c23dc47ce902240072
atbeard <atbeard@me.com>
2020-07-08 16:29:44 -0400

    added a navbar and placeholders

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function getData(){
     var input = $("#search").val()
     $("#gif").empty ();
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"+&api_key=REHW6QysYQ6mkdZjLkzYioQsIL1Nenff&limit=10");
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"+&api_key=REHW6QysYQ6mkdZjLkzYioQsIL1Nenff&limit=5");
     xhr.done(function(data) { 
         console.log("success got data", data);
 

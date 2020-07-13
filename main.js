bd816afc14a7748970386ca297c72d4a249add93
atbeard <atbeard@me.com>
2020-07-13 19:39:34 -0400

    added some comments to better explain the code

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
+//allows the submit button to call the information put in the form
     $("#submit").click( function() {
         $("#outputArea").empty();
         console.log('button clicked');
@@ -7,12 +8,14 @@ $(document).ready( function() {
             data: data,
             url:"http://api.giphy.com/v1/gifs/search",
         })
+//searches through the GIPHY api by going through the JSON
         .done((data)=>{
             let gif = data.data
             $.each(gif, (i, e)=>{
                 var gifs = e.images.original.url
                 console.log(e.images.original.url)
                 var final = '<img src="' +gifs+ '">';
+//appends the gifs to the outputArea ID
                 $('#outputArea').append(final)
             })
         })
